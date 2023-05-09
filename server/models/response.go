f88404f25dd629fc402395863849cb7044c6094e
mutheeew <muthmainnah133@gmail.com>
2023-05-09 16:37:24 +0700

    backend response

--- server/models/response.go
+++ server/models/response.go
@@ -7,8 +7,8 @@ type Response struct {
 	UserId           int
 	User             UserResponse
 	ConsultationId   int
-	ResponseText     string `gorm:"type: text" form:"responseText"`
-	ConsultationLink string `gorm:"type: text" form:"consultationLink"`
+	ResponseText     string `gorm:"type: text" form:"responseText" json:"responseText"`
+	ConsultationLink string `gorm:"type: text" form:"consultationLink" json:"consultationLink"`
 	CreatedAt        time.Time
 	UpdatedAt        time.Time
 }
