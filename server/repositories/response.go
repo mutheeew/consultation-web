6741c09cd3094944724517e2a1c7c6c1b44b525d
mutheeew <muthmainnah133@gmail.com>
2023-05-17 13:38:06 +0700

    fix response

--- server/repositories/response.go
+++ server/repositories/response.go
@@ -29,6 +29,6 @@ func (repo *repository) GetResponse(ID uint) (models.Response, error) {
 
 func (repo *repository) GetAllResponses() ([]models.Response, error) {
 	var response []models.Response
-	err := repo.db.Preload("User").Preload("Consultations").Find(&response).Error
+	err := repo.db.Preload("User").Preload("Consultation").Find(&response).Error
 	return response, err
 }
