870848b8eae908f62445adbce2c9759dd154dddc
mutheeew <muthmainnah133@gmail.com>
2023-05-26 16:51:19 +0700

    Ganti backend

--- client/src/config/api.jsx
+++ client/src/config/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create base URL API
 export const API = axios.create({
   // baseURL: 'http://localhost:5000/api/v1/',
-  baseURL: 'https://consultation-web-production.up.railway.app/api/v1/'
+  baseURL: 'https://consultation-web-production-e07a.up.railway.app/api/v1/'
 });
 
 // Set Authorization Token Header
